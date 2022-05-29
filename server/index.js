3624beb1531e48990cec50edce318ae20b7f60c0
joaochioda <joao.benetasso@taglivros.com>
2022-05-28 21:31:59 -0300

    room system

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 const WebSocket = require("ws");
+
 const wss = new WebSocket.Server({ port: 8080 }, () => {
   console.log("server started");
 });
-wss.on("connection", function connection(ws) {
+
+const roomWaitingPlayers = [];
+const rooms = [];
+
+wss.on("connection", (ws) => {
+  if (roomWaitingPlayers.length === 0) {
+    const idRoom = Math.random() * 10 ** 16;
+    roomWaitingPlayers.push({
+      id: idRoom,
+      players: [ws],
+    });
+  } else {
+    roomWaitingPlayers[0].players.push(ws);
+    rooms.push(roomWaitingPlayers[0]);
+    roomWaitingPlayers.shift();
+
+    rooms[rooms.length - 1].players.forEach((player) => {
+      player.send(JSON.stringify({ type: "room is ready to" }));
+    })
+  }
+  
   ws.on("message", (data) => {
     const stringData = Buffer.from(data).toString();
     console.log("data received \n %o", stringData);
