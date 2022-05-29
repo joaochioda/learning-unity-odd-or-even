fd110461f137499f1a6e7b1f43a1ac71f572fbde
joaochioda <joao.benetasso@taglivros.com>
2022-05-29 10:49:08 -0300

    wip

--- server/index.js
+++ server/index.js
@@ -7,13 +7,21 @@ const wss = new WebSocket.Server({ port: 8080 }, () => {
 const roomWaitingPlayers = [];
 const rooms = [];
 
-wss.on("connection", (ws) => {
+const stateMachine = {
+  "waiting_for_player": 1,
+  "waiting_for_player_pick": 2,
+  "waiting_for_players_number": 3,
+}
+
+function roomLogic(ws) {
+  const idRoom = Math.random() * 10 ** 16;
   if (roomWaitingPlayers.length === 0) {
-    const idRoom = Math.random() * 10 ** 16;
     roomWaitingPlayers.push({
       id: idRoom,
       players: [ws],
+      state: stateMachine["waiting_for_player"],
     });
+    return idRoom;
   } else {
     roomWaitingPlayers[0].players.push(ws);
     rooms.push(roomWaitingPlayers[0]);
@@ -21,13 +29,39 @@ wss.on("connection", (ws) => {
 
     rooms[rooms.length - 1].players.forEach((player) => {
       player.send(JSON.stringify({ type: "room is ready to" }));
-    })
+    });
+
+    const choosePlayerToStart = Math.floor(Math.random() * 2);
+    rooms[rooms.length - 1].players[choosePlayerToStart].send(
+      JSON.stringify({ type: "Pick", message: "Choose odd or even" })
+    );
+    choosePlayerToStart === 0
+      ? rooms[rooms.length - 1].players[1].send(
+          JSON.stringify({ type: "Pick", message: "Waiting player pick" })
+        )
+      : rooms[rooms.length - 1].players[0].send(
+          JSON.stringify({ type: "Pick", message: "Waiting player pick" })
+        );
+    rooms[rooms.length - 1].state = stateMachine["waiting_for_player_pick"];
+    return rooms[rooms.length - 1].id;
   }
-  
+}
+
+function findRoomById(id) {
+  return rooms.find((room) => room.id === id);
+}
+
+wss.on("connection", (ws) => {
+  const idRoom = roomLogic(ws);
+
   ws.on("message", (data) => {
-    const stringData = Buffer.from(data).toString();
-    console.log("data received \n %o", stringData);
-    ws.send(data);
+      const string = Buffer.from(data).toString();
+      const stringToJson = JSON.parse(string);
+      console.log(stringToJson);
+  });
+
+  ws.on('picked', (data) => {
+    console.log(data)
   });
 });
 wss.on("listening", () => {
